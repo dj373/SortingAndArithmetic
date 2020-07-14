6ad8d49eea8ccb2666c58cf5f303b8cedf179858
Dhananjay Sachdeva <dhananjay.sachdeva@gmail.com>
2020-07-14 10:05:50 +0530

    Sorted computations in Descending Order

--- sortingAndArithmeticComputation.sh
+++ sortingAndArithmeticComputation.sh
@@ -9,8 +9,27 @@ computations["c+a/b"]=$(( $c+$a/$b ))
 computations["a%b+c"]=$(( $a%$b+$c ))
 declare -a compArray
 itr=0
+function sortArrayDesc() {
+        for (( ctr1=0; ctr1<${#compArray[@]}; ctr1++ ))
+        do
+                for (( ctr2=0; ctr2<$(( ${#compArray[@]}-$ctr1-1 )); ctr2++ ))
+                do
+                        next=$(( $ctr2+1 ))
+                        if [ ${compArray[ctr2]} -lt ${compArray[next]} ]
+                        then
+                                temp=${compArray[ctr2]}
+                                compArray[$ctr2]=${compArray[next]}
+                                compArray[$next]=$temp
+                        fi
+                done
+        done
+}
+
 for val in ${computations[@]}
 do
 	compArray[(( itr++ ))]=$val
 done
-echo ${compArray[@]}
+echo ${!computations[@]}
+echo ${computations[@]}
+sortArrayDesc ${compArray[@]}
+echo "computations in descending order "${compArray[@]}
